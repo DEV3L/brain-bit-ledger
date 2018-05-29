34299719701e7504f92e440fa3a7ed9e9f046874
DEV3L <jus.beall@gmail.com>
2018-05-28 23:44:06 -0400

    Add enzyme Adapter

---
+++ src/components/widgets/input/Input.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import Input from './Input';
+
+
+describe('<Input />', () => {
+    it('renders Input', () => {
+        const input = shallow(<Input value={'eulav'}/>);
+        expect(input.find('Input').length).toEqual(1);
+        expect(input.prop('value')).toEqual('eulav');
+    });
+});
